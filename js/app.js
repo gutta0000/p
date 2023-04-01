3185f4e3a2e76409bf48e6255591a33c6a8372d3
gutta0000 <129573525+gutta0000@users.noreply.github.com>
2023-04-01 15:30:00 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,25 +1,33 @@
+const apiKey = '6y_Q8qP-ct_RoL98Plgpz5EPef3UryOuLxG-PNT4Uv2XcaJO38q8XStqbglMoQHDE85jS-DHMvOakllEEQNefXiJa2JXESpKivO_s1Mvm0U8cQ5EMfcWETIePscnZHYx'; // 自分のAPIキーに置き換えてください
+
+const options = {
+	method: 'GET',
+	headers: {
+	  'Content-Type': 'application/json',
+	  'Authorization': `Bearer ${apiKey}`
+	},
+	mode: 'cors',
+	credentials: 'include'
+  };
+
 window.addEventListener('load', () => {
-	if ('geolocation' in navigator) {
-	  navigator.geolocation.getCurrentPosition(async (position) => {
-		const { latitude, longitude } = position.coords;
-		const response = await fetch(`https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}&radius=500&sort_by=distance`, {
-		  headers: {
-			Authorization: 'Bearer 6y_Q8qP-ct_RoL98Plgpz5EPef3UryOuLxG-PNT4Uv2XcaJO38q8XStqbglMoQHDE85jS-DHMvOakllEEQNefXiJa2JXESpKivO_s1Mvm0U8cQ5EMfcWETIePscnZHYx'
-		  }
-		});
-		const data = await response.json();
-		if (data.businesses.length > 0) {
-		  const storeName = data.businesses[0].name;
-		  const storeNameElem = document.getElementById('store-name');
-		  storeNameElem.innerText = storeName;
-		} else {
-		  console.log('No nearby stores found');
-		}
-	  }, (error) => {
-		console.error(error);
-	  });
-	} else {
-	  console.error('Geolocation is not supported');
-	}
-  });
-  
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords;
+      const url = `https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}&radius=500&sort_by=distance`;
+      const response = await fetch(url, options);
+      const data = await response.json();
+      if (data.businesses.length > 0) {
+        const storeName = data.businesses[0].name;
+        const storeNameElem = document.getElementById('store-name');
+        storeNameElem.innerText = storeName;
+      } else {
+        console.log('No nearby stores found');
+      }
+    }, (error) => {
+      console.error(error);
+    });
+  } else {
+    console.error('Geolocation is not supported');
+  }
+});
