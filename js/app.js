d64a1f9fb91d528b72b9ab4d055d0943717e6ce0
gutta0000 <129573525+gutta0000@users.noreply.github.com>
2023-04-01 13:22:40 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+	if ('geolocation' in navigator) {
+	  navigator.geolocation.getCurrentPosition(async (position) => {
+		const { latitude, longitude } = position.coords;
+		const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`);
+		const data = await response.json();
+		const storeName = data.address.shop || data.address.amenity || data.address.name || data.address.building || data.address.house_number || 'Unknown';
+		const storeNameElem = document.getElementById('store-name');
+		storeNameElem.innerText = storeName;
+	  }, (error) => {
+		console.error(error);
+	  });
+	} else {
+	  console.error('Geolocation is not supported');
+	}
+  });
+  
