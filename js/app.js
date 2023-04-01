6a71000da605c094b20d0c031fec01e200504714
gutta0000 <129573525+gutta0000@users.noreply.github.com>
2023-04-01 17:26:20 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
 window.addEventListener('load', async () => {
 	if ('geolocation' in navigator) {
-	  try {
-		const { coords: { latitude, longitude } } = await navigator.geolocation.getCurrentPosition();
-		const url = `https://overpass-api.de/api/interpreter?data=[out:json];node(around:50,${latitude},${longitude})[shop];out tags;`
-		const response = await fetch(url);
-		const data = await response.json();
-		const storeName = data.elements[0].tags.name || 'Unknown';
-		const storeNameElem = document.getElementById('store-name');
-		storeNameElem.innerText = storeName;
-	  } catch (error) {
-		console.error(error);
-	  }
+	  navigator.geolocation.getCurrentPosition(
+		async (position) => {
+		  const { coords: { latitude, longitude } } = position;
+		  const url = `https://overpass-api.de/api/interpreter?data=[out:json];node(around:50,${latitude},${longitude})[shop];out tags;`;
+		  try {
+			const response = await fetch(url);
+			const data = await response.json();
+			const storeName = data.elements[0].tags.name || 'Unknown';
+			const storeNameElem = document.getElementById('store-name');
+			storeNameElem.innerText = storeName;
+		  } catch (error) {
+			console.error(error);
+		  }
+		},
+		(error) => {
+		  console.error(error);
+		}
+	  );
 	} else {
 	  console.error('Geolocation is not supported');
 	}
