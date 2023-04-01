2bd3c7ef0a41ece96526cbd3120772f2902411f7
gutta0000 <129573525+gutta0000@users.noreply.github.com>
2023-04-01 17:20:58 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,22 +1,18 @@
-const appId = 'dj00aiZpPThtVTNlWlF5aFdNNCZzPWNvbnN1bWVyc2VjcmV0Jng9MzU-';
-const apiUrl = 'https://map.yahooapis.jp/search/local/V1/localSearch';
-
-// 現在地の緯度経度を取得する
-navigator.geolocation.getCurrentPosition(function(position) {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-
-  // Yahoo!地図APIにリクエストを送信する
-  const requestUrl = apiUrl + '?appid=' + appId + '&lat=' + latitude + '&lon=' + longitude + '&sort=dist';
-  fetch(requestUrl)
-    .then(response => response.text())
-    .then(data => {
-      // レスポンスから一番近いお店の名前を取得する
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(data, 'text/xml');
-      const firstResult = xmlDoc.getElementsByTagName('Result')[0];
-      const name = firstResult.getElementsByTagName('Name')[0].childNodes[0].nodeValue;
-      console.log('一番近いお店の名前は ' + name + ' です。');
-    })
-    .catch(error => console.log(error));
-});
+window.addEventListener('load', async () => {
+	if ('geolocation' in navigator) {
+	  try {
+		const { coords: { latitude, longitude } } = await navigator.geolocation.getCurrentPosition();
+		const url = `https://overpass-api.de/api/interpreter?data=[out:json];node(around:50,${latitude},${longitude})[shop];out tags;`
+		const response = await fetch(url);
+		const data = await response.json();
+		const storeName = data.elements[0].tags.name || 'Unknown';
+		const storeNameElem = document.getElementById('store-name');
+		storeNameElem.innerText = storeName;
+	  } catch (error) {
+		console.error(error);
+	  }
+	} else {
+	  console.error('Geolocation is not supported');
+	}
+  });
+  
