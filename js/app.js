1b547a2528e3a18bac184ab7093daf72352d5fac
gutta0000 <129573525+gutta0000@users.noreply.github.com>
2023-04-01 16:36:10 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,33 +1,35 @@
-const apiKey = 6y_Q8qP-ct_RoL98Plgpz5EPef3UryOuLxG-PNT4Uv2XcaJO38q8XStqbglMoQHDE85jS-DHMvOakllEEQNefXiJa2JXESpKivO_s1Mvm0U8cQ5EMfcWETIePscnZHYx;
-
+const appId = 'fz_ifOexg668ec3stQVTAJCA9OfmHXsc7OQvcQlSllR1OcD2WowgZUTI7PC.eNw-'; // アプリケーションIDを入力してください
 const options = {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${apiKey}`
+    'Authorization': `Bearer ${appId}`
   },
   mode: 'cors',
   credentials: 'include'
 };
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
   if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(async (position) => {
+    try {
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
       const { latitude, longitude } = position.coords;
-      const url = `https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}&radius=500&sort_by=distance`;
+      const url = `https://map.yahooapis.jp/search/local/V1/geoCoder?lat=${latitude}&lon=${longitude}&output=json&sort=geo&results=1&detail=simple&appid=${appId}`;
       const response = await fetch(url, options);
       const data = await response.json();
-      if (data.businesses.length > 0) {
-        const storeName = data.businesses[0].name;
+      if (data.ResultInfo.Count > 0) {
+        const storeName = data.Feature[0].Name;
         const storeNameElem = document.getElementById('store-name');
         storeNameElem.innerText = storeName;
       } else {
-        console.log('No nearby stores found');
+        console.log('近くの店舗が見つかりませんでした。');
       }
-    }, (error) => {
+    } catch (error) {
       console.error(error);
-    });
+    }
   } else {
-    console.error('Geolocation is not supported');
+    console.error('このブラウザではジオロケーションがサポートされていません。');
   }
 });
