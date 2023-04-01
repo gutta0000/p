2707c29b7e4488b7bac0b1d84daad959a0f93408
gutta0000 <129573525+gutta0000@users.noreply.github.com>
2023-04-01 14:47:04 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
 		const { latitude, longitude } = position.coords;
 		const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`);
 		const data = await response.json();
-		const storeName = data.address.shop || data.address.amenity || data.address.name || data.address.building || data.address.house_number || 'Unknown';
+		const storeName = data.address.poi || data.address.shop || data.address.amenity || data.address.name || data.address.building || data.address.house_number || 'Unknown';
 		const storeNameElem = document.getElementById('store-name');
 		storeNameElem.innerText = storeName;
 	  }, (error) => {
