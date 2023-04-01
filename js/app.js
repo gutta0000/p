6d5af6c7173137474bc9dcf6b6b9762b835ef5f2
gutta0000 <129573525+gutta0000@users.noreply.github.com>
2023-04-01 15:17:50 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,11 +2,19 @@ window.addEventListener('load', () => {
 	if ('geolocation' in navigator) {
 	  navigator.geolocation.getCurrentPosition(async (position) => {
 		const { latitude, longitude } = position.coords;
-		const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`);
+		const response = await fetch(`https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}&radius=500&sort_by=distance`, {
+		  headers: {
+			Authorization: 'Bearer 6y_Q8qP-ct_RoL98Plgpz5EPef3UryOuLxG-PNT4Uv2XcaJO38q8XStqbglMoQHDE85jS-DHMvOakllEEQNefXiJa2JXESpKivO_s1Mvm0U8cQ5EMfcWETIePscnZHYx'
+		  }
+		});
 		const data = await response.json();
-		const storeName = data.address.poi || data.address.shop || data.address.amenity || data.address.name || data.address.building || data.address.house_number || 'Unknown';
-		const storeNameElem = document.getElementById('store-name');
-		storeNameElem.innerText = storeName;
+		if (data.businesses.length > 0) {
+		  const storeName = data.businesses[0].name;
+		  const storeNameElem = document.getElementById('store-name');
+		  storeNameElem.innerText = storeName;
+		} else {
+		  console.log('No nearby stores found');
+		}
 	  }, (error) => {
 		console.error(error);
 	  });
